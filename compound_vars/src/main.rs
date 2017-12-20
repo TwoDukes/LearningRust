aa0416280698cb10c43a9160037ec9a3f4b129f9
Unknown <magicdustin14@gmail.com>
2017-12-19 20:24:07 -0800

    new docs file for compound variables

---
+++ compound_vars/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+
+  //TUPLES
+
+    //Tuple destructuring for variable types
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    //Tuple desturucturing for variable value
+    // let tup = (500, 6.4, 1);
+    // let (x, y, z) = tup;
+    // println!("The value of y is: {}", y);
+  
+  //ARRAYS
+
+    //Accessing Array elements
+    // let a = [1, 2, 3, 4, 5];
+    // let first = a[0];
+    // let second = a[1];
+
+    //Invalid Array access
+    // let a = [1, 2, 3, 4, 5];
+    // let index = 10;
+    // let element = a[index];
+    // println!("The value of element is: {}", element);
+
+    
+}
