e9a205707a98454fe4265de04f14ae939e40fb1f
Unknown <magicdustin14@gmail.com>
2017-12-19 20:14:43 -0800

    Small file added as docs for some rust variable structures

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //Mutability
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    //Shadowing
+    // let x = 5;
+
+    // let x = x + 1;
+
+    // let x = x * 2;
+
+    // println!("The value of x is: {}", x);
+
+    //Passing with Shadowing
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+
+    // println!("{}", spaces);
+}
