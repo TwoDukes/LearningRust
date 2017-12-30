faad23e54de550e50716ee04ff95445da40025c2
Unknown <magicdustin14@gmail.com>
2017-12-29 22:16:01 -0800

    Added more to area calculator struct

--- rect_calculator/src/main.rs
+++ rect_calculator/src/main.rs
@@ -32,24 +32,82 @@
 
 //WITH STRUCTS
 //output struct debug tag
+// #[derive(Debug)]
+// struct Rectangle {
+//   width: u32,
+//   height: u32
+// }
+//
+// fn main() {
+//
+//     let rect1 = Rectangle{width: 30, height: 50};
+//
+//     println!("rect1 is {:#?}", rect1);
+//
+//     println!(
+//       "The area of the rectangle is {} square pixels.",
+//       area(&rect1)
+//     );
+// }
+//
+// fn area(rectangle: &Rectangle) -> u32{
+//   rectangle.width * rectangle.height
+// }
+
+
+// //WITH STRUCTS AND METHODS
+// //output struct debug tag
+// #[derive(Debug)]
+// struct Rectangle {
+//   width: u32,
+//   height: u32
+// }
+//
+// impl Rectangle {   
+//   fn area(&self) -> u32{
+//     self.width * self.height
+//   }
+// }
+//
+// fn main() {
+//
+//     let rect1 = Rectangle{width: 30, height: 50};
+//
+//     println!(
+//       "The area of the rectangle is {} square pixels.",
+//       rect1.area()
+//     );
+// }
+
+
+//WITH STRUCTS AND MORE METHODS
+//output struct debug tag
 #[derive(Debug)]
 struct Rectangle {
   width: u32,
   height: u32
 }
 
-fn main() {
+impl Rectangle {   
+  fn area(&self) -> u32{
+    self.width * self.height
+  }
 
-    let rect1 = Rectangle{width: 30, height: 50};
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
 
-    println!("rect1 is {:#?}", rect1);
-
-    println!(
-      "The area of the rectangle is {} square pixels.",
-      area(&rect1)
-    );
+  //associated function that acts as a constructor of sorts
+  fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+  }
 }
 
-fn area(rectangle: &Rectangle) -> u32{
-  rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
